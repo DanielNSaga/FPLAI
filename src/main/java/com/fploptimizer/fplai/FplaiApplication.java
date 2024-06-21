de809b9ef08df84d7235189c3f3df94205a62457
danielsaga <669813@stud.hvl.no>
2024-06-21 21:34:48 +0200

    Initial commit

---
+++ src/main/java/com/fploptimizer/fplai/FplaiApplication.java
@@ -0,0 +1,13 @@
+package com.fploptimizer.fplai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FplaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fploptimizer/fplai/FplaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fploptimizer.fplai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
