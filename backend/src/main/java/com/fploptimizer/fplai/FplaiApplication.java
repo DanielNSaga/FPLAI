45fd4b139423cc3df1c1c5622e086d099d663320
danielsaga <669813@stud.hvl.no>
2024-06-28 17:18:07 +0200

    Created the random forrest AI model

--- backend/src/main/java/com/fploptimizer/fplai/FplaiApplication.java
+++ backend/src/main/java/com/fploptimizer/fplai/FplaiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FplaiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FplaiApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(FplaiApplication.class, args);
     }
 
 }
