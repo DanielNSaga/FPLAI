6ce587bba67480cc7d6738244ec57e4f66a9d495
danielsaga <669813@stud.hvl.no>
2024-08-27 18:05:33 +0200

    Cors config

--- backend/src/main/java/com/fploptimizer/fplai/config/WebConfig.java
+++ backend/src/main/java/com/fploptimizer/fplai/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 // Allow requests from the specified origin (React app running on localhost:3000)
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://fplai.vercel.app/")
                 // Allow the following HTTP methods
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 // Allow credentials such as cookies or HTTP authentication
