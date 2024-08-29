98197bf9089ec186842769b8e86e6f54f07f1f37
danielsaga <669813@stud.hvl.no>
2024-08-29 20:21:36 +0200

    Docker file

--- backend/src/main/java/com/fploptimizer/fplai/config/WebConfig.java
+++ backend/src/main/java/com/fploptimizer/fplai/config/WebConfig.java
@@ -4,36 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Configuration class for setting up CORS (Cross-Origin Resource Sharing) in the application.
- *
- * <p>This class configures CORS settings for the application, allowing the frontend to make requests
- * to the backend API from different origins.</p>
- */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /**
-     * Configures CORS mappings for the application.
-     *
-     * <p>This method allows requests from the specified origins to access the API endpoints under `/api/**`.
-     * It defines which HTTP methods are allowed, enables credentials (such as cookies or HTTP authentication),
-     * and specifies which headers can be included in the requests and responses.</p>
-     *
-     * @param registry the {@link CorsRegistry} used to register CORS mappings.
-     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                // Allow requests from the specified origin
-                .allowedOrigins("*")
-                // Allow the following HTTP methods
+                .allowedOrigins("https://fplai-etf0hmqri-danielnsagas-projects.vercel.app")  // Din frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                // Allow credentials such as cookies or HTTP authentication
-                .allowCredentials(true)
-                // Allow all headers in the requests
                 .allowedHeaders("*")
-                // Expose specific headers in the response, such as "Authorization"
+                .allowCredentials(true)
                 .exposedHeaders("Authorization");
     }
 }
