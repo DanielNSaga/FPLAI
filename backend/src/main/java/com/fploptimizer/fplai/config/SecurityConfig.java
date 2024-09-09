0202cbedeeb914af9355e4746bc6c9fb7fcec3ed
danielsaga <669813@stud.hvl.no>
2024-09-09 16:17:34 +0200

    Docker file

--- backend/src/main/java/com/fploptimizer/fplai/config/SecurityConfig.java
+++ backend/src/main/java/com/fploptimizer/fplai/config/SecurityConfig.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -29,7 +32,7 @@ public class SecurityConfig {
         http
                 .exceptionHandling(c -> c.authenticationEntryPoint(
                         new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for stateless APIs
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Use stateless sessions
                 .authorizeHttpRequests(req -> req.anyRequest().authenticated()) // Require authentication for all requests
                 .httpBasic(httpBasic -> httpBasic.realmName("FPL-Optimizer")); // Updated way to configure HTTP Basic
@@ -40,11 +43,15 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(
-                User.withDefaultPasswordEncoder()
-                        .username(username)
-                        .password(password)
+                User.withUsername(username)
+                        .password(passwordEncoder().encode(password))
                         .roles("USER")
                         .build()
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
