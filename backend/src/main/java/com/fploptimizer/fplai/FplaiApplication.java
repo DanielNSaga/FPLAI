083753b879b2353a5571f5642e18011f384cea4d
danielsaga <669813@stud.hvl.no>
2024-09-09 16:29:02 +0200

    Docker file

--- backend/src/main/java/com/fploptimizer/fplai/FplaiApplication.java
+++ backend/src/main/java/com/fploptimizer/fplai/FplaiApplication.java
@@ -2,12 +2,13 @@ package com.fploptimizer.fplai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class FplaiApplication {
 
-    public static void main(String[] args) {SpringApplication.run(FplaiApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(FplaiApplication.class, args);
     }
-
 }
