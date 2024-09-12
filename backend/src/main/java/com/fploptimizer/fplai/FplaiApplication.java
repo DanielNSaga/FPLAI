4c96da5e4965c3c78ed0bde5457d39d66a46eec8
danielsaga <669813@stud.hvl.no>
2024-09-12 14:39:25 +0200

    Docker file

--- backend/src/main/java/com/fploptimizer/fplai/FplaiApplication.java
+++ backend/src/main/java/com/fploptimizer/fplai/FplaiApplication.java
@@ -4,11 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class FplaiApplication {
 
+    @Autowired
+    private Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(FplaiApplication.class, args);
+    }
+
+    @PostConstruct
+    public void printActiveProfiles() {
+        System.out.println("Active profiles: " + String.join(", ", environment.getActiveProfiles()));
     }
 }
